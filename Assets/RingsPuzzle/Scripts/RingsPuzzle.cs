9c53e598c47e5cd1d834d0e2625618cd910d0027
RodrigoPiresgames <Rodrigo.Pires.gamesdesign@gmail.com>
2023-01-06 18:36:48 +0000

    Add first part of second puzzle

--- Assets/RingsPuzzle/Scripts/RingsPuzzle.cs
+++ Assets/RingsPuzzle/Scripts/RingsPuzzle.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class RingsPuzzle : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private const int STEPS = 12;
+    
+    [SerializeField] private Interactive[] ringObjects;
+    [SerializeField] private Interactive[] buttons;
+
+    private int[] ringStates;
+
+    private void Start()
     {
-        
+        ringStates = new int [3];
+        ringObjects[0].OnInteracted.AddListener(RotateRing1);
+        ringObjects[1].OnInteracted.AddListener(RotateRing2);
+        ringObjects[2].OnInteracted.AddListener(RotateRing3);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void RotateRing(int index)
     {
-        
+        ringStates[index] = (ringStates[index] + 1) % STEPS;
+
+        Vector3 rotation = ringObjects[index].transform.localEulerAngles;
+        rotation.y = ringStates[index] * (360 / STEPS);
+        ringObjects[index].transform.localEulerAngles = rotation;
+    }
+
+    private void RotateRing1 ()
+    {
+        RotateRing(0);
+    }
+
+    private void RotateRing2 ()
+    {
+        RotateRing(1);
+    }
+
+    private void RotateRing3 ()
+    {
+        RotateRing(2);
     }
 }
